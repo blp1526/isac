4f74f50e94e1dc21d2ce1c569850a7d5a8fd0cd7
Shingo Kawamura <blp1526@gmail.com>
2017-11-26 16:12:19 +0900

    Add loadConfig

--- lib/isac.go
+++ lib/isac.go
@@ -32,5 +32,13 @@ func New(configPath string, verbose bool, zones string) *Isac {
 func (i *Isac) Run() (err error) {
 	i.logger.Debugf("configPath: %s", i.configPath)
 	i.logger.Debugf("zones: %s", i.zones)
+
+	config, err := loadConfig(i.configPath)
+	if err != nil {
+		return err
+	}
+
+	i.logger.Debugf("AccessToken: %s", config.AccessToken)
+	i.logger.Debugf("AccessTokenSecret: %s", config.AccessTokenSecret)
 	return nil
 }
