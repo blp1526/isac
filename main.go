8d7dbb85f1274b7178c19ba356e9a57fa6a170fa
Shingo Kawamura <blp1526@gmail.com>
2017-12-03 21:21:34 +0900

    Add app.Version

--- main.go
+++ main.go
@@ -22,6 +22,7 @@ func main() {
 	app := cli.NewApp()
 	app.Name = "isac"
 	app.Usage = "interactive SAKURA Cloud"
+	app.Version = version
 	app.Authors = []cli.Author{
 		cli.Author{
 			Name:  "Shingo Kawamura",
